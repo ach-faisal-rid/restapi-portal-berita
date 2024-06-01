3eef65a066d4da93ba2058e8394919251716e31b
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-06-01 21:10:44 +0700

    get posts telah selesai

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    // list posts
+    public function index() {
+        $posts = Post::all();
+        return response()->json($posts);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', function() {
-    dd('ini test read portal berita');
-});
+Route::get('/posts', [PostController::class, 'index']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
