69be3d3c4128f7d87578fc6e4505d313525e8311
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-06-01 21:07:05 +0700

    test untuk list posts

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/posts', function() {
+    dd('ini test read portal berita');
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
