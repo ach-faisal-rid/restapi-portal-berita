f7f838ca81d3959613939e49803df5e0ab24723b
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-06-02 13:35:30 +0700

    menambahkan tambah data store pada post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\PostResource;
 use App\Http\Resources\PostDetailResource;
 
@@ -19,6 +20,20 @@ class PostController extends Controller
     public function show($id) {
         $post = Post::with('writer:id,username')->findOrFail($id);
         return new PostDetailResource($post);
+    }
+
+    // create new post
+    public function store(Request $request) {
+        // dd($request->all());
+
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'news_content' => 'required',
+        ]);
+
+        $request['author'] = Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->group(function() {
-    Route::post('/logout', [AuthenticationController::class, 'logout']);
+
+    // management user
     Route::get('/current', [AuthenticationController::class, 'current']);
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
+
+    // post
+    Route::post('/posts',[PostController::class, 'store']);
 });
 
 Route::get('/posts', [PostController::class, 'index']);
