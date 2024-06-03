c5e00a6bc4156035c4caf3c28a52c252e9d3f11c
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-06-03 15:30:04 +0700

    membuat agar email yang terdaftar itu static

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'email' => $this->faker->unique()->safeEmail(),
-            'username' => $this->faker->unique()->userName(),
-            'firstname' => $this->faker->firstName(),
-            'lastname' => $this->faker->lastName(),
+            'email' => 'marlee79@gmail.com',
+            'username' => 'marlee.bradke',
+            'firstname' => 'Micaela',
+            'lastname' => 'Moen',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
