82eba29ae24eddc4a30a7343bf455254ea0f9d22
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-06-04 12:38:47 +0700

    menambahkan proses update image

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
-use App\Http\Resources\PostDetailResource;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Resources\PostDetailResource;
 
 class PostController extends Controller
 {
@@ -32,8 +33,17 @@ class PostController extends Controller
             'news_content' => 'required',
         ]);
 
+        $imagePath = null;
+
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('public/posts'); // Simpan gambar dalam direktori 'public/posts'
+        }
+
+        $requestData = $request->all();
         $request['author'] = Auth::user()->id;
-        $post = Post::create($request->all());
+        $requestData['image'] = $imagePath;
+
+        $post = Post::create($requestData);
         return new PostDetailResource($post->loadMissing('writer:id,username'));
     }
 
@@ -53,7 +63,20 @@ class PostController extends Controller
         $validator = Validator::make($request->all(), [
             'title' => 'required|string|max:255',
             'news_content' => 'required|string',
+            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
+
+        // Jika ada perubahan gambar
+        if ($request->hasFile('image')) {
+            // Menghapus gambar lama jika ada
+            if ($post->image) {
+                Storage::delete($post->image);
+            }
+
+            // Menyimpan gambar baru
+            $imagePath = $request->file('image')->store('public/posts');
+            $post->image = $imagePath;
+        }
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
@@ -69,7 +92,7 @@ class PostController extends Controller
 
     // delete atau soft delete
     public function destroy($id) {
-        $post = Post::find($id);
+        $post = Post::withTrashed()->find($id);
 
         if (!$post) {
             return response()->json(['message' => "Post with ID $id not found"], 404);
